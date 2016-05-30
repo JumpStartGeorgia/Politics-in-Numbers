24beb2ee217a2e2b05e27e9fd4ffdbe32198cebd
jason <jason.addie@gmail.com>
2016-05-30 14:06:49 +0400

    fix staging deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Starter-Template-Production'
-set :repository, 'git@github.com:JumpStartGeorgia/Starter-Template.git'
+set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
+set :branch, 'master'
 set :web_url, ENV['PRODUCTION_WEB_URL']
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'pin-staging'
 set :application, 'Politics-In-Numbers-Staging'
 set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
-set :branch, 'master'
-set :web_url, dev-pin.jumpstart.ge
+set :branch, 'deploy'
+set :web_url, 'dev-pin.jumpstart.ge'
 set :visible_to_robots, false
