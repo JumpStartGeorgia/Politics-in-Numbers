e40b68cddf61c004600d0becdfe359791b3fa654
Antarya <antarya@gmail.com>
2016-10-12 17:53:06 +0400

    Adding url to production deploy file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :user, 'pin'
 set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
 set :branch, 'master'
-set :web_url, ENV['PRODUCTION_WEB_URL']
+set :web_url, "live.pins.ge"
