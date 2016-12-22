f23b302645bf0d36c57a311174b27ce9f0827d10
Antarya <antarya@gmail.com>
2016-12-22 16:58:02 +0400

    Production branch updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :domain, 'pins.ge'
 set :user, 'pin'
 set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
-set :branch, 'master'
+set :branch, 'embed'
 set :web_url, "live.pins.ge"
