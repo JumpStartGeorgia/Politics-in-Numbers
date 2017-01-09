bb6243ddee62d2b1b1811105bee6220858b287c5
Antarya <antarya@gmail.com>
2017-01-09 19:31:57 +0400

    smtp fix for production

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,9 +2,9 @@ if Rails.env.production? || Rails.env.staging?
   Rails.application.config.action_mailer.delivery_method = :smtp
 
   Rails.application.config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: 'box.pins.ge',
     port: 587,
-    domain: 'jumpstart.ge',
+    domain: 'pins.ge',
     user_name: ENV['APPLICATION_FEEDBACK_FROM_EMAIL'],
     password: ENV['APPLICATION_FEEDBACK_FROM_PWD'],
     authentication: :plain,
