e19355ea0597b1f1829d1e8ad58be8290eda938b
Antarya <antarya@gmail.com>
2017-02-01 17:27:06 +0400

    Staging branch updated to donation_amount

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :user, 'pin-staging'
 set :application, 'Politics-In-Numbers-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
-set :branch, 'embed'
+set :branch, 'donation_amount'
 set :web_url, 'dev-pin.jumpstart.ge'
 set :visible_to_robots, false
 set :use_ssl, true
