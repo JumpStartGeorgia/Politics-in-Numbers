3b3cf3380858a495f3259174ccbb030dbdfc8c02
Antarya <antarya@gmail.com>
2017-03-21 12:38:42 +0400

    Adding threads to puma

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,5 @@ set :application, 'Politics-In-Numbers'
 set :repository, 'https://github.com/JumpStartGeorgia/Politics-in-Numbers.git'
 set :branch, 'embed'
 set :web_url, "pins.ge"
+set :puma_thread_count_min, '1'
+set :puma_thread_count_max, '8'
